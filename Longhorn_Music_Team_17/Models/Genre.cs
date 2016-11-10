82c0a2a82b59ebeeab7202b7505d9e451f60363d
Quinten <qwyble01@gmail.com>
2016-11-10 17:06:35 -0600

    ?

---
+++ Longhorn_Music_Team_17/Models/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Longhorn_Music_Team_17.Models
+{
+    public class Genre
+    {
+        public Int32 GenreID { get; set; }
+        public string GenreName { get; set; }
+
+        //navigational properties
+        public virtual List<Artist> Artists { get; set; }
+        public virtual List<Album> Albums { get; set; }
+        public virtual List<Song> Songs { get; set; }
+    }
+}
---
+++ Longhorn_Music_Team_17/Models/Rating.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Longhorn_Music_Team_17.Models
+{
+    public class Rating
+    {
+        public Int32 RatingID { get; set; }
+
+        public Int32 RatingScore { get; set; }
+
+        public virtual List<Song> Songs { get; set; }
+    }
+}
