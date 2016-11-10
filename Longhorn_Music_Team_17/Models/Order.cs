ed64f6322431b97aa052074d740872b3328df000
Quinten <qwyble01@gmail.com>
2016-11-10 17:06:35 -0600

    ?

---
+++ Longhorn_Music_Team_17/Models/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Longhorn_Music_Team_17.Models
+{
+    public class Order
+    {
+        public Int32 OrderID { get; set; }
+
+        public Decimal PurchasePrice { get; set; } //is this correct?
+
+        public DateTime OrderDate { get; set; }
+
+        public Decimal Discount { get; set; }
+
+        public OrderStatus Status { get; set; }
+
+        public enum OrderStatus
+        {
+            Cart,
+            Checkout,
+            Completed
+        }
+
+        //navigational properties
+        public virtual User User { get; set; }
+        public virtual List<Song> Songs { get; set; }
+        public virtual List<Album> Albums { get; set; }
+        public virtual OrderDetail OrderDetail { get; set; } //each order can have only one order detail
+    }
+}
