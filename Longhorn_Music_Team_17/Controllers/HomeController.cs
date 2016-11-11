397528ea32c8be6df654bfef01184d1a78f9fed9
Quinten <qwyble01@gmail.com>
2016-11-10 18:50:22 -0600

    ?

--- Longhorn_Music_Team_17/Controllers/HomeController.cs
+++ Longhorn_Music_Team_17/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using Longhorn_Music_Team_17.Models;
 using System.Data.Entity;
 
 namespace Longhorn_Music_Team_17.Controllers
-{
+{//
     public class HomeController : Controller
     {
         AppDbContext db = new AppDbContext();
